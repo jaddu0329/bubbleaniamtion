5256c311580e14f36c33bfec8cb375c0b3015af8
jadeja krishnarajsinh <jkrishnaraj4@gmail.com>
2026-04-17 23:21:19 +0530

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BubbleEffect from './bubble'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BubbleEffect />
+  </React.StrictMode>
+)
